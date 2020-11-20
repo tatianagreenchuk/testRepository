c04d4a2c75eefdedced4c83fce949dd2932e480e
VladimirKing <cheremuha1@gmail.com>
2020-11-20 13:01:32 +0300

    Vova

--- src/edu/lits/CreateTask.java
+++ src/edu/lits/CreateTask.java
@@ -1,5 +1,22 @@
 package edu.lits;
 
+import java.util.Scanner;
+
 public class CreateTask {
+    public static void main(String[] args) {
+        int nz =2;
+        int n;
+        System.out.println("1+Enter - вийти з програми");
+        System.out.println("2+Enter - вивчати нові слова");
+        while (nz!=1){
+            Scanner n1=new Scanner(System.in);
+            n=n1.nextInt();
+            if (n==2){
+            System.out.println("вивчаємо нові слова");
+        }else {
+                break;
+            }
+        }
+    }
 
 }
--- src/edu/lits/Main.java
+++ src/edu/lits/Main.java
@@ -1,9 +1,8 @@
 package edu.lits;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Nazar");
         System.out.println("Tatiana");
         System.out.println("Serhii");
