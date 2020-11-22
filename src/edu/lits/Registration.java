1d7bf661bc1b98eb70868fd0c1dfaae29df8e9e5
Serhiikon <kondrserg@gmail.com>
2020-11-22 11:38:58 +0200

    language chandged to english

--- src/edu/lits/Registration.java
+++ src/edu/lits/Registration.java
@@ -7,10 +7,10 @@ public class Registration {
         String menuOption = "0";
         while ("0".equals(menuOption)) {
             System.out.println();
-            System.out.println("<меню реєстрації>");
-            System.out.println("1: повернутись назад");
-            System.out.println("2: зареєструватися");
-            System.out.print("виберіть опцію: ");
+            System.out.println("<registration menu>");
+            System.out.println("1: go back");
+            System.out.println("2: start registration");
+            System.out.print("choose option: ");
             Scanner textInput = new Scanner(System.in);
             menuOption = textInput.nextLine();
             switch (menuOption) {
@@ -18,42 +18,42 @@ public class Registration {
                     break;
                 case "2":
                     System.out.println();
-                    System.out.println("<введіль ваші дані>");
-                    System.out.print("Логін: ");
+                    System.out.println("<please enter your data>");
+                    System.out.print("Login: ");
                     String login = textInput.nextLine();
-                    System.out.print("Пароль: ");
+                    System.out.print("Password: ");
                     String password = textInput.nextLine();
-                    System.out.print("повторіть Пароль: ");
+                    System.out.print("repeat Password: ");
                     String passwordRepeat = textInput.nextLine();
 
                     String completeReg = "0";
                     while ("0".equals(completeReg)) {
                         System.out.println();
-                        System.out.println("<створення облікового запису>");
-                        System.out.println("1: завершити реєстрацію");
-                        System.out.println("2: відміна реєстрації");
-                        System.out.print("виберіть опцію: ");
+                        System.out.println("<complete registration>");
+                        System.out.println("1: complete registration and create account");
+                        System.out.println("2: cancel registration");
+                        System.out.print("choose option: ");
                         completeReg = textInput.nextLine();
                         switch (completeReg) {
                             case "1":
                                 System.out.println();
-                                System.out.println("Вітаємо! Реєстрація пройшла успішно");
+                                System.out.println("Congratulations! Registration was successful");
                                 break;
                             case "2":
                                 System.out.println();
-                                System.out.println("реєстрація відмінена");
+                                System.out.println("registration has been canceled");
                                 menuOption = "0";
                                 break;
                             default:
                                 System.out.println();
-                                System.out.println("невідома опція спробуйте ще раз");
+                                System.out.println("unknown option please try again");
                                 completeReg = "0";
                         }
                     }
                     break;
                 default:
                     System.out.println();
-                    System.out.println("невідома опція спробуйте ще раз");
+                    System.out.println("unknown option please try again");
                     menuOption = "0";
             }
         }
