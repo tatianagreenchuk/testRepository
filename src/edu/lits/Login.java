30b6e6da62b34a0c96bdb97aead3a7362f63b8cc
VladimirKing <cheremuha1@gmail.com>
2020-11-25 21:22:02 +0300

    work to go

--- src/edu/lits/Login.java
+++ src/edu/lits/Login.java
@@ -4,45 +4,40 @@ import java.util.Scanner;
 
 public class Login {
     public static void main(String[] args) {
-        String menu = "0";
-        while (menu.equals("0")) {
-            System.out.println("1.Новачок");
-            System.out.println("2.Вже є свій аккаунт");
-            Scanner scan = new Scanner(System.in);
-            String account = scan.nextLine();
-            if (account.equals("1")) {
-                System.out.println("Зареєструватися");
-                break;
-            }
-            if (account.equals("2")) {
-                System.out.println("1. Увійти в свій аккаунт ");
-                System.out.println("2. Повернутися в попереднє меню ");
-                String name = scan.nextLine();
-                if (name.equals("1")) {
-                    System.out.print("Введіть свій логін: ");
-                    String login = scan.next();
-                    System.out.print("Введіть свій пароль: ");
-                    String password = scan.next();
-                    String[] pass = new String[]{"aaaaa", "sssss", "ddddd", "fffff", "ttttt"};
-                    switch (password) {
-                        case "aaaaa":
-                        case "sssss":
-                        case "ddddd":
-                        case "fffff":
-                        case "ttttt":
-                            System.out.println("Вітаємо, ви увійшли в свій аккаунт!");
-                            break;
-                        default:
-                            System.out.println("Виникла помилка, спробуйте ще раз");
-                    }}
+        login();
 
-                if (name.equals("2")) {
-                }
-            }
-            {
+    }
+
+    public static void login() {
+            System.out.println("1. Увійти в свій аккаунт ");
+            System.out.println("2. Повернутися в попереднє меню ");
+            Scanner scan=new Scanner(System.in);
+
+            String name = scan.nextLine();
+            if (name.equals("1")) {
+                System.out.print("Введіть свій логін: ");
+                String login = scan.next();
+                System.out.print("Введіть свій пароль: ");
+                String password = scan.next();
+                String[] pass = new String[]{"aaaaa", "sssss", "ddddd", "fffff", "ttttt"};
+                switch (password) {
+                    case "aaaaa":
+                    case "sssss":
+                    case "ddddd":
+                    case "fffff":
+                    case "ttttt":
+                        System.out.println("Вітаємо, ви увійшли в свій аккаунт!");
+                        break;
+                    default:
+                        System.out.println("Виникла помилка, спробуйте ще раз");
+                }}
+        Main bacK=new Main();
+            if (name.equals("2")) {
+                bacK.open();
             }
         }
     }
-}
+
+
 
 
