48d7a79923f28f3b61a65884fab919377f577e7e
Serhiikon <kondrserg@gmail.com>
2020-11-28 14:14:28 +0200

    вставив runRegistration() в Main клас

--- src/edu/lits/Main.java
+++ src/edu/lits/Main.java
@@ -6,26 +6,21 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Nazar");
-        System.out.println("Tatiana");
-        System.out.println("Serhii");
-        System.out.println("Vovchik");
         open();
-
     }
 
        public static void open() {
         Login loG=new Login();
         String menu = "0";
         while (menu.equals("0")) {
+            System.out.println();
             System.out.println("1.Новачок");
             System.out.println("2.Вже є свій аккаунт");
             System.out.println("3.Завершити");
             Scanner scan = new Scanner(System.in);
             String account = scan.nextLine();
             if (account.equals("1")) {
-                Registration.registration();
+                Registration.runRegistration();
                 break;
             }
             if (account.equals("2")) {
