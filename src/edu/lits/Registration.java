83bd4e5feb620f6ef4d2160084dd422d9a0fe04a
Serhiikon <kondrserg@gmail.com>
2020-12-03 15:40:02 +0200

    переробив клас Registration, метод runRegistration() перейменував на createAccount()

--- src/edu/lits/Registration.java
+++ src/edu/lits/Registration.java
@@ -3,45 +3,29 @@ package edu.lits;
 import java.util.Scanner;
 
 public class Registration {
-    /*static final String HEADER1 = "<registration menu>";
-    static final String CHOOSE = "choose option: ";
-    static final String HEADER2 = "<please enter your data>";
-    static final String LOGIN = "Login: ";
-    static final String PASSWORD = "Password: ";
-    static final String REPEATE = "repeat Password: ";
-    static final String HEADER3 = "<complete registration>";
-    static final String CONGRAT = "Congratulations! Registration was successful";
-    static final String CANCELED = "registration has been canceled";*/
-
-    static final String HEADER1 = "<меню реєстрації>";
     static final String CHOOSE = "виберіть опцію: ";
-    static final String HEADER2 = "<будь-ласка введіть ваші дані>";
-    static final String LOGIN = "Логін: ";
-    static final String PASSWORD = "Пароль: ";
-    static final String REPEATE = "повторіть Пароль: ";
-    static final String HEADER3 = "<завершення реєстрації>";
-    static final String CONGRAT = "Вітаємо! Реєстрація пройшла успішно";
-    static final String CANCELED = "реєстрація була відмінена";
+    static final String LOGIN = "Ваш новий логін: ";
+    static final String PASSWORD = "Ваш новий пароль: ";
+    static final String REPEAT = "повторіть Ваш Пароль: ";
 
     static boolean stayInMenu;
 
-    public static void runRegistration() {
+    public static void createAccount() {
 
         stayInMenu = true;
 
         while (stayInMenu) {
 
             System.out.println();
-            System.out.println(HEADER1);
-            System.out.println(RegMenu1.BACK.menuNumber + " - " + RegMenu1.BACK.menuName);
-            System.out.println(RegMenu1.START.menuNumber + " - " + RegMenu1.START.menuName);
+            System.out.println(RegMenu.CREATE.menuNumber + " - " + RegMenu.CREATE.menuName);
+            System.out.println(RegMenu.BACK.menuNumber + " - " + RegMenu.BACK.menuName);
             System.out.print(CHOOSE);
 
             Scanner textInput = new Scanner(System.in);
             String menuOption = textInput.nextLine();
 
-            RegMenu1 menu = RegMenu1.BACK;
-            for (RegMenu1 i: RegMenu1.values()) {
+            RegMenu menu = RegMenu.BACK;
+            for (RegMenu i: RegMenu.values()) {
                 if (menuOption.equals(i.menuNumber)) {
                     menu = i;
                 }
@@ -49,55 +33,19 @@ public class Registration {
 
             switch (menu) {
                 case BACK:
-                    Main.open();
+                    stayInMenu = false;
                     break;
-                case START:
-                    createAccount();
-            }
-        }
-    }
-
-    public static void createAccount() {
-
-        System.out.println();
-        System.out.println(HEADER2);
-
-        Scanner textInput = new Scanner(System.in);
-
-        System.out.print(LOGIN);
-        String login = textInput.nextLine();
-        System.out.print(PASSWORD);
-        String password = textInput.nextLine();
-        System.out.print(REPEATE);
-        String passwordRepeat = textInput.nextLine();
-
-        boolean stayInSubMenu = true;
-        while (stayInSubMenu) {
-            System.out.println();
-            System.out.println(HEADER3);
-            System.out.println(RegMenu2.COMPLETE.menuNumber + " - " + RegMenu2.COMPLETE.menuName);
-            System.out.println(RegMenu2.CANCEL.menuNumber + " - " + RegMenu2.CANCEL.menuName);
-            System.out.print(CHOOSE);
-
-            String menuOption = textInput.nextLine();
-
-            RegMenu2 menu = RegMenu2.CANCEL;
-            for (RegMenu2 i: RegMenu2.values()) {
-                if (menuOption.equals(i.menuNumber)) {
-                    menu = i;
-                }
-            }
-
-            switch (menu) {
-                case COMPLETE:
+                case CREATE:
                     System.out.println();
-                    System.out.println(CONGRAT);
-                    Navigation.navigation();
-                    break;
-                case CANCEL:
+                    System.out.print(LOGIN);
+                    String login = textInput.nextLine();
+                    System.out.print(PASSWORD);
+                    String password = textInput.nextLine();
+                    System.out.print(REPEAT);
+                    String passwordRepeat = textInput.nextLine();
                     System.out.println();
-                    System.out.println(CANCELED);
-                    stayInSubMenu = false;
+                    Navigating.navigating();
+                    stayInMenu = false;
             }
         }
     }
