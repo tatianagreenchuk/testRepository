7e593d5a27c1a845e7b67eaa195ac6a480f0a36f
VladimirKing <cheremuha1@gmail.com>
2020-12-09 01:03:40 +0300

    new bakery, there are two classes

--- src/edu/lits/Login.java
+++ src/edu/lits/Login.java
@@ -1,5 +1,7 @@
 package edu.lits;
 
+import edu.lits.UserArrayFile.ArrayFileUser;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -23,6 +25,14 @@ public class Login {
                 System.out.println();
                 System.out.println("Введіть свій логін: ");
                 String login = scan.nextLine();
+
+                ArrayFileUser arrayFileUser=new ArrayFileUser();
+                arrayFileUser.userUser();
+                for (String s :arrayFileUser.arrayUserName){
+                if(login.equals(s)){
+                    System.out.println("ви зареєстровані у нас");
+                }}
+
                 BufferedReader reader = new BufferedReader(new FileReader("User.txt"));
                 String fileString = reader.readLine();
 
