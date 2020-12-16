1eb75df7e8c629129aaa2911ffffe8a34b0f9d68
Serhiikon <kondrserg@gmail.com>
2020-12-16 17:47:38 +0200

    відредагував клас Testing, тепер при виборі пункту Пройти тестування будуть виводитися всі завдання зі списку taskList

--- src/edu/lits/Testing.java
+++ src/edu/lits/Testing.java
@@ -2,6 +2,8 @@ package edu.lits;
 
 import java.util.Scanner;
 
+import edu.lits.model.Task;
+import edu.lits.model.Answer;
 import static edu.lits.model.WriterFileUser.writerFile;
 
 public class Testing {
@@ -19,7 +21,13 @@ public class Testing {
                     case "1":
                         writerFile.add("запускаємо тестування");
                         System.out.println();
-                        System.out.println("запускаємо тестування ...");
+                        for (Task i : Main.taskList) {
+                            System.out.println(i.getNumber() + ". " + i.getText());
+                            for (Answer j : i.answerList) {
+                                System.out.println(j.getNumber() + ") " + j.getText());
+                            }
+                            System.out.println();
+                        }
                         break;
                     case "2":
                         break;
